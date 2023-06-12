31f2cc79aa1a11d4c67b71cafbb3a0ba109e6767
tsotsd <tsotsd8@gmail.com>
2023-06-13 00:10:17 +0800

    added hw-2.js

---
+++ hw-2.js
@@ -0,0 +1,68 @@
+// Задание 1
+// let a = 10;
+// alert(a);
+// a = 20;
+// alert(a);
+
+// Задание 2
+// iPhoneOneYear = 2007;
+// alert(iPhoneOneYear);
+
+// Задание 3
+// nameCreateJs = 'Brendan Eich';
+// alert(nameCreateJs);
+
+// Задание 4
+// let a = 10;
+// let b = 2;
+// alert(a+b);
+// alert(a-b);
+// alert(a*b);
+// alert(a/b);
+
+// Задание 5
+// let result = 2 ** 5;
+// alert(result);
+
+// Задание 6
+// let a = 9;
+// let b = 2;
+// alert(a % b);
+
+// Задание 7
+// let num = 1;
+// num = num++ + 5;
+// num = num-- - 3;
+// num = ++num * 7;
+// num = --num / 3;
+// num = --num + 1;
+// num = --num - 1;
+// alert(num);
+
+// Задание 8
+// let age = Number(prompt('Сколько Вам лет?'));
+// alert(age);
+
+// Задание 9
+// const user = {
+//         name: 'Bob',
+//         age: 32,
+//         isAdmin: true
+// };
+
+// Задание 9.1
+// user.['city of residence'] = 'Moscow';
+
+// Задание 9.2
+// user.age = 23;
+
+// Задание 9.3
+// delete user.['city of residence'];
+
+// Задание 9.4
+// let info = prompt("Какую информацию хотите узнать о пользователе?", name);
+// alert(user[info]);
+
+// Задание 10
+// let name = prompt("What is your name?");
+// alert(`Привет,` ${name});
