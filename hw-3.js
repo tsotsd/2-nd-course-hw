188406ae978907496561d535a4052b1ae9c6178c
tsotsd <tsotsd8@gmail.com>
2023-06-14 14:27:17 +0800

    add hw-3

---
+++ hw-3.js
@@ -0,0 +1,9 @@
+let password = 'пароль';
+let enterPaswd = prompt('Введите пароль');
+if (password === enterPaswd) {
+    alert('Пароль введен верно');
+
+} else {
+    alert('Пароль введен неправильно');
+
+}
