ececd92fd09e35dedcd5dd859a5f84417421074a
tsotsd <tsotsd8@gmail.com>
2023-06-15 20:15:10 +0800

    finish hw-3

--- hw-3.js
+++ hw-3.js
@@ -1,3 +1,4 @@
+// Задание 1
 // let password = 'пароль';
 // let enterPaswd = prompt('Введите пароль');
 // if (password === enterPaswd) {
@@ -5,5 +6,65 @@
 
 // } else {
 //     alert('Пароль введен неправильно');
+// }
 
+// Задание 2
+// let c = 2;
+// (c > 0 && c < 10) ? console.log('Верно') : console.log('Неверно');
+
+// Задание 3
+// let d = 101;
+// let e = 99;
+// (d > 100 || e > 100) ? console.log('Верно') : console.log('Неверно');
+
+// Задание 4
+// let a = '2';
+// let b = '3';
+// a = Number(a);
+// b = Number(b);
+// alert(a+b);
+
+// Задание 5
+// let monthNumber = Number(prompt('Введите номер месяца в году'));
+
+// switch (monthNumber) {
+//     case 1:
+//         console.log('winter')
+//         break;
+//     case 2:
+//         console.log('winter')
+//         break;
+//     case 3:
+//         console.log('spring')
+//         break;
+//     case 4:
+//         console.log('spring')
+//         break;
+//     case 5:
+//         console.log('spring')
+//         break;
+//     case 6:
+//         console.log('summer')
+//         break;
+//     case 7:
+//         console.log('summer')
+//         break;
+//     case 8:
+//         console.log('summer')
+//         break;
+//     case 9:
+//         console.log('autumn')
+//         break;
+//     case 10:
+//         console.log('autumn')
+//         break;
+//     case 11:
+//         console.log('autumn')
+//         break;
+//     case 12:
+//         console.log('winter')
+//         break;
+//     default:
+//         console.log('номер месяца больше 13');
+//         break;
 // }
