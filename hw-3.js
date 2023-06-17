ef009caa96aea0e9776720d3ff1cec0f8bb13b2b
tsotsd <tsotsd8@gmail.com>
2023-06-17 17:16:33 +0800

    1

--- hw-3.js
+++ hw-3.js
@@ -3,7 +3,7 @@ let password = 'пароль';
 let enterPaswd = prompt('Введите пароль');
 if (password === enterPaswd) {
     alert('Пароль введен верно');
-
+}
 // } else {
 //     alert('Пароль введен неправильно');
 // }
