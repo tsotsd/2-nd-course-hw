2dae966555dedab9e9c4417a3e0c75f6f2c01b50
tsotsd <135599352+tsotsd@users.noreply.github.com>
2023-06-21 11:59:16 +0800

    hw-5  exercize2

--- hw-5.js
+++ hw-5.js
@@ -1,4 +1,5 @@
-// //Задание 1
+// Задание 1
+// ------------------------
 // function less(a, b) {
 //     if (a > b) {
 //         console.log(`${a}`);
@@ -10,4 +11,28 @@
 // less(4, 5);
 // less(9, 7);
 // less(1, 1);
+// --------------------------
 
+// function less() {
+//     let first = +prompt('Enter the first number');
+//     let second = +prompt('Enter the second number');
+
+//     if (first > second) {
+//         alert(second);
+//     } else {
+//         alert(first);
+//     }
+// }
+
+// less();
+// -------------------------------
+// Задание 2
+// function returnString() {
+//     let number = +prompt('enter the number');
+//     if (number % 2 == 0) {
+//         alert('Число четное');
+//     } else {
+//         alert('Число нечетное');
+//     }
+// }
+// returnString();
