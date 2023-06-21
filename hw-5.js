6938a8ea9b0403a11a6e5e6bb0625324bb5d2d32
tsotsd <135599352+tsotsd@users.noreply.github.com>
2023-06-21 10:35:26 +0800

    added exercize 1 to hw-5

--- hw-5.js
+++ hw-5.js
@@ -1,1 +1,13 @@
-//Задание 1
+// //Задание 1
+// function less(a, b) {
+//     if (a > b) {
+//         console.log(`${a}`);
+//     } else {
+//         console.log(`${b}`);
+//     }
+// }
+
+// less(4, 5);
+// less(9, 7);
+// less(1, 1);
+
