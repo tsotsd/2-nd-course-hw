518a11060f4a45ba2d77977085a672cd39500a3d
tsotsd <135599352+tsotsd@users.noreply.github.com>
2023-06-21 17:16:58 +0800

    added exercize2 hw-5

--- hw-5.js
+++ hw-5.js
@@ -18,19 +18,36 @@
 //     let second = +prompt('Enter the second number');
 
 //     if (first > second) {
-//         alert(second);
-//     } else {
-//         alert(first);
+//         return second;
+//     } else if (first <= second) {
+//         return first;
+//     } else { 
+//         return('incorrect data');
 //     }
 // }
 
-// less();
+// alert(less());
 // -------------------------------
+// function less() {
+//     let first = +prompt('Enter the first number');
+//     let second = +prompt('Enter the second number');
+
+//        let min = first;
+
+//        if (min > second) {
+//            min = second; 
+//        }
+//        return min;
+//     }
+// alert(less());
+// --------------------------------------
 // Задание 2
 // function returnString() {
 //     let number = +prompt('enter the number');
-//     if (number % 2 == 0) {
+//     if (number % 2 === 0) {
 //         alert('Число четное');
+//     } else if (isNaN(number)) {
+//         alert('incorrect data')
 //     } else {
 //         alert('Число нечетное');
 //     }
