172549605fe301f888ebb128195c8807fa1b78e5
tsotsd <135599352+tsotsd@users.noreply.github.com>
2023-06-22 23:49:38 +0800

    added exercise 7

--- hw-5.js
+++ hw-5.js
@@ -53,3 +53,105 @@
 //     }
 // }
 // returnString();
+// -----------------------------------------
+// Задание 3.1
+// function calcSquare(square) {
+//     let result = square * square    
+//     console.log(result);
+// }
+
+// calcSquare(8);
+
+// Задание 3.2
+// function calcSquare(square) {
+//     let result = square * square    
+//     alert(result);
+// }
+
+// calcSquare(5);
+// ---------------------------------------------
+// Задание 4
+// function checkYaer() {
+//     let question = +prompt('How old are you');
+//     if (question >= 13) {
+//         alert('Welcome');
+//     } else if (question <= 12 && question > 0) {
+//         alert('Hi, friend')
+//     } else {
+//         alert('You entered an invalid value')
+//     }
+// }
+
+// checkYaer();
+// ------------------------------------------
+// Задание 5 
+// function calcMult() {
+//     let first = +prompt('enter the first number');
+//     let second = +prompt('enter the second number');
+    
+//     if (isNaN(first) || isNaN(second)) {
+//         alert('One or both values are not a number');
+//     } else {
+//         alert (first * second);
+//     }
+// }
+
+// calcMult();
+// -------------------------------------------
+// function calcMult(first, second) {
+   
+//     if (isNaN(first) || isNaN(second)) {
+//         console.log('One or both values are not a number');
+//     } else {
+//         console.log(first * second);
+//     }
+// }
+
+// calcMult('df','sw');
+// calcMult(5,8);
+// calcMult(5,'fv');
+// ----------------------------------------
+// Задание 6
+
+// function getCube() {
+//     let number = +prompt('Enter the number');
+//     cube = Math.pow(number, 3);
+//     if (isNaN(number)) {
+//         return;
+//     } else {
+//         alert(`n в кубе равняется ${cube}`);
+//     }
+// }
+// getCube();
+// ------------------------------------------
+// Задание 7
+// const circle1 = {
+//     radius: 5,
+//     getArea: function() {
+//         let area = `${circle1.radius}` * `${circle1.radius}` * 3.14;
+//         console.log(area);
+//     },
+//     getPerimetr: function() {
+//         let perimetr = 2 * 3.14 * `${circle1.radius}`;
+//         console.log(perimetr);
+//     }
+// }
+
+// const circle2 = {
+//     radius: 8,
+//     getArea: function() {
+//         let area = `${circle2.radius}` * `${circle2.radius}` * 3.14;
+//         console.log(area);
+//     },
+//     getPerimetr: function() {
+//         let perimetr = 2 * 3.14 * `${circle2.radius}`;
+//         console.log(perimetr);
+//     }
+// }
+
+// circle1.getArea();
+// circle1.getPerimetr();
+// circle2.getArea();
+// circle2.getPerimetr();
+// --------------------------------------------
+[]
