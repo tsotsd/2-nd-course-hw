3b65801554528dfb846f5f37726b1f79698a1573
tsotsd <135599352+tsotsd@users.noreply.github.com>
2023-06-25 01:22:04 +0800

    Добавил задания с  1 по 4

---
+++ hw-6.js
@@ -0,0 +1,43 @@
+// Задание 1
+// const array = [1, 5, 4, 10, 0, 3]
+// for (let index = 0; index < array.length; index++) {
+// 	if (array[index] === 10) break;
+// 	console.log(array[index]);
+// }
+// --------------------------------
+// const array = [1, 5, 4, 10, 0, 3]
+// let index = 0;
+// while (array[index] != 10) {
+// 	console.log(array[index]);
+// 	index++;
+// }
+// ----------------------------------
+// const array = [1, 5, 4, 10, 0, 3]
+// let index = 0;
+// do {
+// 	console.log(array[index]);
+// 	index++;
+// } while (array[index] != 10) 
+//  ---------------------------------
+// Задание 2 
+// const array = [1, 5, 4, 10, 0, 3]
+// console.log(array.indexOf(4));
+// -------------------------------------
+// Задание 3
+// let array = [1, 3, 5, 10, 20];
+// array = array.join(' ');
+// console.log(array);
+// ----------------------------------
+// Задание 4
+// let arr = [];
+
+// for (let i = 0; i < 3; i++) {
+// 	arr[i] = []; 
+	
+// 	for (let j = 0; j < 3; j++) {
+// 		arr[i].push(1); 
+// 	}
+// }
+
+// console.log(arr);
+// -----------------------------
