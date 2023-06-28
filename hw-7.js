786cac5d487cd78e10fcc8011e88e6fe2bed259c
tsotsd <135599352+tsotsd@users.noreply.github.com>
2023-06-28 17:59:34 +0800

    Задание 1

---
+++ hw-7.js
@@ -0,0 +1,18 @@
+// Задание 1
+// let str = 'string';
+// str = str.toUpperCase();
+// console.log(str);
+// --------------------------
+// Задание 2
+// const searchStart = (['Кошка', 'Кит', 'Комар', 'Носорог'], 'ко'); // ['кошка', 'комар']
+// const search = 'ко';
+
+// searchStart.forEach((el) => {
+// 	if (el.toLowerCase().includes(search.toLowerCase)) {
+// 		console.log(el);
+// 	}
+// });
+
+// //searchStart(['яблоко', 'груша', 'гриб', 'огурец'], 'гру'); // ['груша']
+// //searchStart(['Дом', 'Банк', 'Больница', 'Театр'], 'Кино'); // []
+
