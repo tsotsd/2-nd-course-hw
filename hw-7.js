606b3bb8f441f92a2af0c36daba7a037969d5f81
tsotsd <135599352+tsotsd@users.noreply.github.com>
2023-06-29 00:13:03 +0800

    Задание 1-5

--- hw-7.js
+++ hw-7.js
@@ -15,4 +15,21 @@
 
 // //searchStart(['яблоко', 'груша', 'гриб', 'огурец'], 'гру'); // ['груша']
 // //searchStart(['Дом', 'Банк', 'Больница', 'Театр'], 'Кино'); // []
+// -------------------------------------------
+// Задание 3
+// let number = 32.58884;
+// let minNumber = Math.floor(number);
+// let maxNumber = Math.ceil(number);
+// let roundNumber = Math.round(number);
+// console.log(minNumber, maxNumber, roundNumber);
+// -----------------------------------------
+// Задание 4
+// console.log(Math.max(52, 53, 49, 77, 21, 32));
+// console.log(Math.min(52, 53, 49, 77, 21, 32));
+// ---------------------------------------
+// Задание 5
+// function getRandomInt() {
+//     return Math.round(Math.random() * 10);
+//  }
+//  console.log(getRandomInt());
 
