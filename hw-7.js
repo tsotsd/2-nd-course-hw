219a06174fd8931ac16f8ac2d0d7501dd8bc0c9b
tsotsd <135599352+tsotsd@users.noreply.github.com>
2023-06-29 17:54:49 +0800

    Решил до 9 задания

--- hw-7.js
+++ hw-7.js
@@ -32,4 +32,42 @@
 //     return Math.round(Math.random() * 10);
 //  }
 //  console.log(getRandomInt());
+// -------------------------------------------
+// Задание 6 - ?
+// function getRandomArrNumbers(n) {
 
+//     let arrWithNum = [];
+    
+//     for(i = 0; i < n/2.5; i++) {
+//        arrWithNum.push(Math.floor(Math.random(n) * 10));
+//     }
+    
+//     return arrWithNum;
+// }
+
+// console.log(getRandomArrNumbers(7));
+// ------------------------------------
+// Задание 7
+// function getRandomInt(minValue, maxValue) {
+//     return Math.round(Math.random() * (maxValue- minValue)) + minValue;
+//   }
+//   console.log(getRandomInt(1, 10));
+// ----------------------------------
+// Задание 8
+// let currentDate = new Date();
+// console.log(currentDate);
+// ------------------------------
+// let myDate = new Date(1688023937848);
+// console.log(myDate); 
+// --------------------------------
+// let myDate = new Date(2023, 5, 29, 15, 41, 20, 10); 
+// console.log(myDate); 
+// -----------------------------------
+// Задание 9
+// let currentDate = new Date(); 
+// let futureDate = currentDate.getDate() + currentDate.setDate(73); 
+// console.log(futureDate);
+
+// let myDate = new Date (1691833942054);
+// console.log(myDate);
+// // let futureDate = 'Через 73 дня после текущей наступит' + currentDate.getDate() 
